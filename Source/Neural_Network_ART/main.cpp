fa0273c76d76446d1e324c8a8e8b881a7050c86a
DmitryKyssa <qualitygamecore.dcissa@gmail.com>
2023-12-21 17:44:56 +0200

    Update row in table

--- Source/Neural_Network_ART/main.cpp
+++ Source/Neural_Network_ART/main.cpp
@@ -32,7 +32,7 @@ int main()
 	//std::streambuf* last = std::cout.rdbuf(buffer.rdbuf());
 
 	int trainingPass = 0;
-	while (!trainData.getEOF() || myNet.getError() < 0.05)
+	while (!trainData.getEOF())
 	{
 		trainingPass++;
 		std::cout << std::endl << "Pass:" << trainingPass;
@@ -99,50 +99,87 @@ int main()
 	//	std::cout << columnsNames.at(i) << std::endl;
 	//}
 
-	//std::string checkIfRowExists = "SELECT EXISTS(SELECT 1 FROM myTbl WHERE u_tag=" + myNet.getName() +" LIMIT 1);";
-	//exec = sqlite3_prepare_v3(database, checkIfRowExists.c_str(), (int)checkIfRowExists.length(), 0, &stmt, 0);
-	//std::cout << "Check if row exists: " << exec << std::endl;
-	//sqlite3_step(stmt);
-	//sqlite3_finalize(stmt);
+	std::string checkIfRowExists = "SELECT EXISTS(SELECT 1 FROM nn-info WHERE Name=" + myNet.getName() +" LIMIT 1);";
+	exec = sqlite3_prepare_v3(database, checkIfRowExists.c_str(), (int)checkIfRowExists.length(), 0, &stmt, 0);
+	std::cout << "Check if row exists: " << exec << std::endl;
+	sqlite3_step(stmt);
+	sqlite3_finalize(stmt);
 
 	ss.str("");
-	ss << "INSERT INTO nn_info (";
-	for (size_t i = 0; i < columnsNames.size(); i++) {
-		if (i == columnsNames.size() - 1) {
-			ss << columnsNames.at(i) << ")";
-			continue;
+	if (exec == 0) {
+		ss << "INSERT INTO nn_info (";
+		for (size_t i = 0; i < columnsNames.size(); i++) {
+			if (i == columnsNames.size() - 1) {
+				ss << columnsNames.at(i) << ")";
+				continue;
+			}
+			ss << columnsNames.at(i) << ", ";
 		}
-		ss << columnsNames.at(i) << ", ";
-	}
-	ss << " VALUES ('" << myNet.getName() << "', " << std::to_string(myNet.getError()) << ",";
-	for (size_t l = 0; l < myNet.getLayers().size(); l++) {
-		for (size_t n = 0; n < myNet.getLayers().at(l).size(); n++) {
-			ss << std::to_string(myNet.getLayers().at(l).at(n).getOutput()) << ",";
-			ss << "'";
-			if (myNet.getLayers().at(l).at(n).getOutputWeights().size() == 0) {
-				ss << " ";
-			}
-			for (size_t w = 0; w < myNet.getLayers().at(l).at(n).getOutputWeights().size(); w++) {
-				ss << " " << myNet.getLayers().at(l).at(n).getOutputWeights().at(w).weight;
-			}
-			if ((l == myNet.getLayers().size() - 1) && (n == myNet.getLayers().at(l).size() - 1)) {
+		ss << " VALUES ('" << myNet.getName() << "', " << std::to_string(myNet.getError()) << ",";
+		for (size_t l = 0; l < myNet.getLayers().size(); l++) {
+			for (size_t n = 0; n < myNet.getLayers().at(l).size(); n++) {
+				ss << std::to_string(myNet.getLayers().at(l).at(n).getOutput()) << ",";
 				ss << "'";
-			} else
-			ss << "',";
+				if (myNet.getLayers().at(l).at(n).getOutputWeights().size() == 0) {
+					ss << " ";
+				}
+				for (size_t w = 0; w < myNet.getLayers().at(l).at(n).getOutputWeights().size(); w++) {
+					ss << " " << myNet.getLayers().at(l).at(n).getOutputWeights().at(w).weight;
+				}
+				if ((l == myNet.getLayers().size() - 1) && (n == myNet.getLayers().at(l).size() - 1)) {
+					ss << "'";
+				}
+				else
+					ss << "',";
+			}
 		}
+		ss << ");";
+		std::cout << ss.str() << std::endl;
+
+		std::string insert = ss.str();
+
+		exec = sqlite3_prepare_v3(database, insert.c_str(), (int)insert.length(), 0, &stmt, 0);
+		std::cout << "Add data: " << exec << std::endl;
+		exec = sqlite3_step(stmt);
+		sqlite3_finalize(stmt);
 	}
-	ss << ");";
-	std::cout << ss.str() << std::endl;
+	else {
+		ss << "UPDATE nn_info SET Error = " << myNet.getError() << ",";
+		int columnIndex = 2;
+		for (size_t l = 0; l < myNet.getLayers().size(); l++) {
+			for (size_t n = 0; n < myNet.getLayers().at(l).size(); n++) {
+				ss << columnsNames.at(columnIndex) << " = " << std::to_string(myNet.getLayers().at(l).at(n).getOutput()) << ",";
+				columnIndex++;
+				if (myNet.getLayers().at(l).at(n).getOutputWeights().size() == 0) {
+					ss << " ";
+				}
+				ss << columnsNames.at(columnIndex) << " = ";
+				ss << "'";
+				for (size_t w = 0; w < myNet.getLayers().at(l).at(n).getOutputWeights().size(); w++) {
+					ss << " " << myNet.getLayers().at(l).at(n).getOutputWeights().at(w).weight;
+				}
+				columnIndex++;
+				if ((l == myNet.getLayers().size() - 1) && (n == myNet.getLayers().at(l).size() - 1)) {
+					ss << "'";
+				}
+				else
+					ss << "',";
+			}
+		}
+		ss << "WHERE Name = '" << myNet.getName() << "';";
 
-	std::string insert = ss.str();
+		std::cout << ss.str() << std::endl;
 
-	exec = sqlite3_prepare_v3(database, insert.c_str(), (int)insert.length(), 0, &stmt, 0);
-	std::cout << "Add data: " << exec << std::endl;
-	exec = sqlite3_step(stmt);
-	sqlite3_finalize(stmt);
+		std::string update = ss.str();
+
+		exec = sqlite3_prepare_v3(database, update.c_str(), (int)update.length(), 0, &stmt, 0);
+		std::cout << "Update data: " << exec << std::endl;
+		//std::cout << sqlite3_errmsg(database) << std::endl;
+		exec = sqlite3_step(stmt);
+		sqlite3_finalize(stmt);
+	}
 	
 	//exec = sqlite3_bind_text(stmt, 1, buffer.str().c_str(), (int)buffer.str().length(), SQLITE_STATIC);
-	std::cout << "Evaluate statement: " << exec << std::endl;
 
 	exec = sqlite3_close(database);
 	std::cout << "Close database: " << exec << std::endl;
