78759ae178c5da56212ae2281cc05196a06582fa
DmitryKyssa <131445209+DmitryKyssa@users.noreply.github.com>
2024-03-24 14:29:20 +0200

    [not fixxed] with big nn all outputs are equal with 1

--- Source/Neural_Network_ART/main.cpp
+++ Source/Neural_Network_ART/main.cpp
@@ -24,54 +24,54 @@ int main() {
 	std::string valueString = "STRING";
 	std::string alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-	std::string query = "(ID INT PRIMARY KEY NOT NULL, STRING TEXT NOT NULL);";
-	db.createTable(tableForTests, query);
-	tableForTests += " (ID,STRING) ";
-	query = "";
-	for (int i = 0; i < TESTS_NUMBER; i++) {
-		int stringLength = rand() % 1000 + 9000;
-		query = "VALUES (" + std::to_string(i + 1) + ", '" + gen.generateRandomString(stringLength, alphanumeric) + "'); ";
-		db.insert(tableForTests, query);
-		std::cout << "String added! " << i << std::endl;
-	}
+	//std::string query = "(ID INT PRIMARY KEY NOT NULL, STRING TEXT NOT NULL);";
+	//db.createTable(tableForTests, query);
+	//tableForTests += " (ID,STRING) ";
+	//query = "";
+	//for (int i = 0; i < TESTS_NUMBER; i++) {
+	//	int stringLength = rand() % 1000 + 9000;
+	//	query = "VALUES (" + std::to_string(i + 1) + ", '" + gen.generateRandomString(stringLength, alphanumeric) + "'); ";
+	//	db.insert(tableForTests, query);
+	//	std::cout << "String added! " << i << std::endl;
+	//}
 
-	//std::string test = db.getTestByID(tableForTests, valueString, 1);
-	//std::string str = "Hello world";
-	//char search = str[0];
-	//std::string substr = str.substr(1, 11);
+	std::string test = db.getTestByID(tableForTests, valueString, 1);
+	std::string str = "Hello world";
+	char search = str[0];
+	//std::string substr = test.substr(5, 500);
 
-	//std::vector<unsigned int> topology = { (unsigned int)str.length(), 20, (unsigned int)str.length() };
-	//Net* net = new Net(topology);
+	std::vector<unsigned int> topology = { (unsigned int)str.length(), 5, (unsigned int)str.length() };
+	Net* net = new Net(topology);
 
-	//std::vector<double> input = StringNormalizer::normalize(str);
-	////std::vector<double> target = StringNormalizer::findOneChar(input, search, net->getLayers().back().size());
-	//std::vector<double> target = StringNormalizer::findSubstring(str, substr, net->getLayers().back().size());
+	std::vector<double> input = StringNormalizer::normalize(str);
+	std::vector<double> target = StringNormalizer::findOneChar(input, search, net->getLayers().back().size());
+	//std::vector<double> target = StringNormalizer::findSubstring(test, substr, net->getLayers().back().size());
 	//std::cout << "Targets: " << target << std::endl;
 
-	//size_t i = 0;
-	//std::string result = "";
+	size_t i = 0;
+	std::string result = "";
 	//while (result.find(substr) == -1)
-	//	//while (result.find(search) == -1)
-	//	//while(i < 20)
-	//{
-	//	net->forwardPropagation(input);
-	//	net->backPropagation(target);
-	//	for (size_t j = 2; j < net->getLayers().size(); j++)
-	//	{
-	//		std::cout << "Layer #" << j + 1 << std::endl;
-	//		for (size_t k = 0; k < net->getLayers().at(j).size(); k++)
-	//		{
-	//			std::cout << net->getLayers().at(j).at(k).getOutput() << " ";
-	//		}
-	//		std::cout << std::endl;
-	//	}
-	//	std::vector<double> outputNeurons = net->getResults();
-	//	result = StringNormalizer::convertToString(outputNeurons);
-	//	std::cout << "Converted string: " << result << std::endl;
-	//	std::cout << "Error " << i + 1 << ": " << net->getError() << std::endl;
-	//	std::cout << std::endl;
-	//	i++;
-	//}
+	while (result.find(search) == -1)
+		//while(i < 20)
+	{
+		net->forwardPropagation(input);
+		net->backPropagation(target);
+		for (size_t j = 2; j < net->getLayers().size(); j++)
+		{
+			std::cout << "Layer #" << j + 1 << std::endl;
+			for (size_t k = 0; k < net->getLayers().at(j).size(); k++)
+			{
+				std::cout << net->getLayers().at(j).at(k).getOutput() << " ";
+			}
+			std::cout << std::endl;
+		}
+		std::vector<double> outputNeurons = net->getResults();
+		result = StringNormalizer::convertToString(outputNeurons);
+		std::cout << "Converted string: " << result << std::endl;
+		std::cout << "Error " << i + 1 << ": " << net->getError() << std::endl;
+		std::cout << std::endl;
+		i++;
+	}
 
 	return 0;
 }
